45898c3db49e1866d5b8177ec9458f01bc655517
Eshwer2002 <101573822+Eshwer2002@users.noreply.github.com>
2022-07-28 22:02:11 +0530

    Added initial SaveEarth files

---
+++ script.js
@@ -0,0 +1,13 @@
+
+let navbar = document.querySelector(".navbar");
+let menuBtn = document.querySelector("#menu-btn");
+
+menuBtn.onclick = function(){
+  menuBtn.classList.toggle("fa-times");
+  navbar.classList.toggle("active")
+};
+let input = document.querySelector('.amount input');
+input.onclick = function(){
+  input.document.addEventListener('oninput', function(){
+  });
+}
